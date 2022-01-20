b260b3d30aad7e63526f8c990d146a108fa66726
Gabriel Romay Machado <romay.gabriel@gmail.com>
2022-01-20 16:48:45 +0100

    Use Google Maps JS API to display a map as background for the app

--- src/components/templates/CreateJob.tsx
+++ src/components/templates/CreateJob.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { styled } from "@mui/system";
 
@@ -7,7 +7,6 @@ import Toaster from "../atoms/Toaster";
 
 function CreateJobTemplate() {
   const Map = styled("div")({
-    backgroundColor: "purple", // TODO: temp
     position: "fixed",
     width: "100%",
     height: "100%",
@@ -15,9 +14,33 @@ function CreateJobTemplate() {
     top: 0,
   });
 
+  useEffect(() => {
+    new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: 41.0082, lng: 28.9784 },
+      zoom: 8,
+    });
+  }, []);
+
   return (
     <Map>
-      <AddressesForm />
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          left: 0,
+          top: 0,
+        }}
+        id="map"
+      />
+
+      <AddressesForm
+        styles={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+        }}
+      />
       <Toaster message="Job has been created successfully!" />
     </Map>
   );
