167464c408bb1c21779867a954c3735b8e4851a9
Gabriel Romay Machado <romay.gabriel@gmail.com>
2022-01-21 10:53:06 +0100

    Config babel to use async functions; call graphql api to geocode address instead of mocked data

--- src/components/pages/CreateJob.tsx
+++ src/components/pages/CreateJob.tsx
@@ -1,21 +1,10 @@
 import React, { useState } from "react";
 
+import { geocodeAddressRequest } from "../../api/geocode";
+
 import CreateJobTemplate from "../templates/CreateJob";
 
 function CreateJobPage() {
-  const POSITIONS = {
-    pickUp: {
-      address: "29 Rue Du 4 Septembre",
-      lat: 48.86985,
-      lng: 2.33457,
-    },
-    dropOff: {
-      address: "15 Rue de Bourgogne",
-      lat: 48.85908,
-      lng: 2.31804,
-    },
-  };
-
   const [positionsState, setPositionsState] = useState({
     pickUp: { state: "blank", value: "" },
     dropOff: { state: "blank", value: "" },
@@ -28,29 +17,31 @@ function CreateJobPage() {
     });
   };
 
-  const geocodeAddress = (data) => {
-    console.log(
-      "GEOCODING...",
-      data,
-      data.currentTarget.id,
-      data.currentTarget.value
-    );
+  const geocodeAddress = ({ currentTarget }) => {
+    const id = currentTarget.id;
 
-    const target = data.currentTarget;
-
-    const position = POSITIONS[target.id];
-
-    if (position.address === target.value) {
-      setPositionsState({
-        ...positionsState,
-        [target.id]: { state: "present", lat: position.lat, lng: position.lng },
-      });
-    } else {
-      setPositionsState({
-        ...positionsState,
-        [target.id]: { state: "error" },
-      });
-    }
+    geocodeAddressRequest(currentTarget.value).then((result) => {
+      if (result.errors) {
+        setPositionsState({
+          ...positionsState,
+          [id]: {
+            ...positionsState[id],
+            state: "error",
+          },
+        });
+      } else {
+        const geocode = result.data.geocode;
+        setPositionsState({
+          ...positionsState,
+          [id]: {
+            ...positionsState[id],
+            state: "present",
+            lat: geocode.latitude,
+            lng: geocode.longitude,
+          },
+        });
+      }
+    });
   };
 
   return (
