b59e7783726c66c407ae8a90c3a2f18b0b6e5353
Gabriel Romay Machado <romay.gabriel@gmail.com>
2022-02-15 12:11:55 +0100

    TypeScript types fixes

--- src/components/organisms/AddressesForm.tsx
+++ src/components/organisms/AddressesForm.tsx
@@ -8,7 +8,7 @@ import Button from "../atoms/Button";
 import FormBox from "../atomsStyled/FormBox";
 import IconTextInput from "../molecules/IconTextInput";
 
-interface Props {
+interface AddressesFormProps {
   styles: object;
   formState: FormState;
   createJobState: string | null;
@@ -18,10 +18,54 @@ interface Props {
   createJob: Function;
 }
 
-interface IconsPaths {
-  pickUp: { [key: string]: string };
-  dropOff: { [key: string]: string };
+interface InputProps {
+  id: string;
+  label: string;
+  formState: { [key: string]: string };
+  positionsState: { [key: string]: PositionState };
+  setPosition: Function;
+  geocodeAddress: Function;
 }
+
+interface IconsPaths {
+  [key: string]: { [key: string]: string };
+}
+
+const ICONS_PATH_TEMPLATE = (file: string) => `src/assets/${file}.svg`;
+const ICONS_PATHS: IconsPaths = {
+  pickUp: {
+    blank: ICONS_PATH_TEMPLATE("pickUpBadgeBlank"),
+    present: ICONS_PATH_TEMPLATE("pickUpBadgePresent"),
+    error: ICONS_PATH_TEMPLATE("pickUpBadgeError"),
+  },
+  dropOff: {
+    blank: ICONS_PATH_TEMPLATE("dropOffBadgeBlank"),
+    present: ICONS_PATH_TEMPLATE("dropOffBadgePresent"),
+    error: ICONS_PATH_TEMPLATE("dropOffBadgeError"),
+  },
+};
+
+const Input = ({
+  id,
+  label,
+  formState,
+  positionsState,
+  setPosition,
+  geocodeAddress,
+}: InputProps) => (
+  <IconTextInput
+    id={id}
+    iconPath={ICONS_PATHS[id][positionsState[id].status]}
+    alt={`${label} badge with grey background`}
+    placeholder={`${label} address`}
+    styles={{ marginBottom: "16px" }}
+    value={formState[id]}
+    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+      setPosition(id, event.target.value);
+    }}
+    geocodeAddress={geocodeAddress}
+  />
+);
 
 function AddressesForm({
   styles,
@@ -31,46 +75,23 @@ function AddressesForm({
   setPosition,
   geocodeAddress,
   createJob,
-}: Props) {
-  const ICONS_PATH_TEMPLATE = (file: string) => `src/assets/${file}.svg`;
-  const ICONS_PATHS: IconsPaths = {
-    pickUp: {
-      blank: ICONS_PATH_TEMPLATE("pickUpBadgeBlank"),
-      present: ICONS_PATH_TEMPLATE("pickUpBadgePresent"),
-      error: ICONS_PATH_TEMPLATE("pickUpBadgeError"),
-    },
-    dropOff: {
-      blank: ICONS_PATH_TEMPLATE("dropOffBadgeBlank"),
-      present: ICONS_PATH_TEMPLATE("dropOffBadgePresent"),
-      error: ICONS_PATH_TEMPLATE("dropOffBadgeError"),
-    },
-  };
-
+}: AddressesFormProps) {
   return (
     <FormBox style={styles}>
-      {/* TODO: create a DRY function to render these inputs */}
-      <IconTextInput
+      <Input
         id="pickUp"
-        iconPath={ICONS_PATHS.pickUp[positionsState.pickUp.status]}
-        alt="Pick Up badge with grey background"
-        placeholder={"Pick up address"}
-        styles={{ marginBottom: "16px" }}
-        value={formState.pickUp}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          setPosition("pickUp", event.target.value);
-        }}
+        label="Pick Up"
+        formState={formState}
+        positionsState={positionsState}
+        setPosition={setPosition}
         geocodeAddress={geocodeAddress}
       />
-      <IconTextInput
+      <Input
         id="dropOff"
-        iconPath={ICONS_PATHS.dropOff[positionsState.dropOff.status]}
-        alt="Drop off badge with grey background"
-        placeholder={"Drop off address"}
-        styles={{ marginBottom: "16px" }}
-        value={formState.dropOff}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          setPosition("dropOff", event.target.value);
-        }}
+        label="Drop Off"
+        formState={formState}
+        positionsState={positionsState}
+        setPosition={setPosition}
         geocodeAddress={geocodeAddress}
       />
       <Button
