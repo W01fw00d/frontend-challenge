2bae835ee9913731e0523b110e96c79bcb605c78
Gabriel Romay Machado <romay.gabriel@gmail.com>
2022-02-16 10:54:01 +0100

    Add ThemeProvider to tests to fix them

--- src/components/atoms/Button.test.tsx
+++ src/components/atoms/Button.test.tsx
@@ -4,13 +4,17 @@
 
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@mui/material/styles";
 
+import theme from "../themes/original";
 import Button from "./Button";
 
 describe("Button", () => {
   test("Displays Button label", () => {
     render(
-      <Button label="This is a button" disabled={false} onClick={() => {}} />
+      <ThemeProvider theme={theme}>
+        <Button label="This is a button" disabled={false} onClick={() => {}} />
+      </ThemeProvider>
     );
 
     expect(screen.getByText("This is a button")).toBeInTheDocument();
--- src/components/atoms/Toaster.test.tsx
+++ src/components/atoms/Toaster.test.tsx
@@ -4,13 +4,17 @@
 
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@mui/material/styles";
 
+import theme from "../themes/original";
 import Toaster from "./Toaster";
 
 describe("Toaster", () => {
   test("Displays Toaster text", () => {
     render(
-      <Toaster isOpen={true} message="This is a Toaster" close={() => {}} />
+      <ThemeProvider theme={theme}>
+        <Toaster isOpen={true} message="This is a Toaster" close={() => {}} />
+      </ThemeProvider>
     );
 
     expect(screen.getByText("This is a Toaster")).toBeInTheDocument();
